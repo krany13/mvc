8603272d07e4984495133b664501706a455c0a4a
Vladimir Krasniy <krasniy.vladim@yandex.ru>
2022-05-15 11:07:31 +0300

    1 commit

---
+++ RunGroopWebApp/RunGroopWeb/Interfaces/IPhotoService.cs
@@ -0,0 +1,11 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace RunGroopWebApp.Interfaces
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> AddPhotoAsync(IFormFile formFile);
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
+
+    }
+}
---
+++ RunGroopWebApp/RunGroopWeb/ViewModels/CreateClubViewModel.cs
@@ -0,0 +1,13 @@
+﻿using RunGroopWebApp.Data.Enum;
+
+namespace RunGroopWebApp.ViewModels
+{
+    public class CreateClubViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+        public ClubCategory ClubCategory { get; set; }
+    }
+}
